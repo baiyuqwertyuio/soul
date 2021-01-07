5d02d21450b4a847400b95f7a5443690a5332d50
Dony <1758020778@qqcom>
2021-01-07 14:10:07 +0800

    1234

---
+++ src/components/sliding.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    登录后的页面
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "sliding"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import login from '@/components/login'
+import sliding from '@/components/sliding'
+import registry from '@/components/registry'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/sliding',
+      name: 'sliding',
+      component: sliding
+    },
+    {
+      path:'/registry',
+      name:'registry',
+      component: registry
     }
+
   ]
 })
